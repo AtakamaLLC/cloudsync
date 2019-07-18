fdecf7e6a716594b5529f00ab4a1d1c308fca3e0
erik <erik@q32.com>
2019-07-18 14:44:39 -0400

    test_events.py

--- tests/test_events.py
+++ tests/test_events.py
@@ -4,57 +4,43 @@ from pycloud import EventManager
 
 @pytest.fixture
 def mock_gdrive():
+    # return a manager linked to mock-provider gdrive
     return 'a'
 
 @pytest.fixture
 def mock_dropbox():
+    # return a manager linked to mock-provider gdrive
     return 'b'
 
 @pytest.fixture(params=['gdrive', 'dropbox'])
-def provider(request, gdrive, dropbox):
+def manager(request, gdrive, dropbox):
     return {'gdrive': mock_gdrive, 'b': mock_dropbox}[request.param]
 
 @pytest.fixture
-def env():
+def util():
     return None
 
-def test_event_basic(env, manager):
-    temp = env.temp_file(fill_bytes=32)
+def test_event_basic(util, manager):
+    temp = util.temp_file(fill_bytes=32)
     cloud_id1, hash1 = provider.upload(temp, "/dest")
 
-    manager.sync
-    for e in .events(timeout=1):
-        if e is None:
-            break
+    # this is normally a blocking function that runs forever
+    def done():
+        return os.path.exists(local_path)
 
-        assert e.path = "/dest"
-        assert e.cloud_id
-        assert e.mtime
-        assert e.exists
-        assert e.source = Event.REMOTE
+    # loop the sync until the file is found
+    manager.sync(timeout=1, until=done)
 
-    provider.delete(cloud_id=e.cloud_id)
+    local_path = manager.local_path("/fandango")
 
-    with pytest.raises(CloudFileNotFoundError):
-        provider.delete(cloud_id=e.cloud_id)
-   
-    for e in provider.events(timeout=1):
-        if e is None:
-            break
+    util.fill_bytes(local_path, count=32)
 
-        assert e.path = "/dest"
-        assert e.cloud_id
-        assert e.mtime
-        assert not e.exists
-        assert e.source = Event.REMOTE
+    manager.local_event(path=local_path, exists=True)
 
-def test_api_failure(provider):
-    # assert that the cloud 
-    # a) uses an api function
-    # b) does not trap CloudTemporaryError's
+    # loop the sync until the file is found
+    manager.sync(timeout=1, until=done)
 
-    with patch.object(provider, "api", side_effect=lambda *a, **k: raise CloudTemporaryError("fake disconned")):
-        with pytest.raises(CloudTemporaryError):
-            provider.exists("/notexists")
+    info = provider.info("/fandango")
 
-
+    assert info.hash == provider.local_hash(temp)
+    assert info.cloud_id
