82babed011b5c578676bba738213b32b02b29e19
erik <erik@q32.com>
2019-07-18 13:40:30 -0400

    .

---
+++ pycloud/__init__.py
@@ -0,0 +1,36 @@
+"""
+
+pycloud enables simple cloud file-level sync with a variety of cloud providers
+
+External modules:
+
+pycloud.Event
+pycloud.Provider
+pycloud.Sync
+
+Example:
+
+import pycloud
+
+prov = pycloud.Provider('GDrive', token="237846234236784283")
+
+uuid, hash = prov.upload(file, "/dest")
+
+Command-line example:
+
+pycloud -p gdrive --token "236723782347823642786" -f ~/gdrive-folder --daemon
+
+"""
+
+__version__ = "0.1.1"
+
+# import modules into top level for convenience
+
+from .provider import *
+from .event import *
+from .sync import *
+
+from .command import main
+
+if __name__ == "__main__":
+    main()
