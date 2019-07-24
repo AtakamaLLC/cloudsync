6948a2783c9b8488e9c6016505cc02b218f95d3c
earthman1 <30826762+earthman1@users.noreply.github.com>
2019-07-24 15:50:04 -0400

    Test not found condition (#7)
    * .
    
    * .
    
    * .
    
    * improve file exists test
    
    * .
    
    * .

--- tests/integration/test_provider.py
+++ tests/integration/test_provider.py
@@ -116,6 +116,7 @@ def test_create_upload_download(util, provider):
 
 
 def test_rename(util, provider: Provider):
+    # TODO: test that renaming the parent folder renames the children
     dat = os.urandom(32)
 
     def data():
@@ -206,9 +207,9 @@ def test_event_basic(util, provider: Provider):
     assert path == dest
     assert received_event.mtime
     assert received_event.exists
-    provider.delete(oid=received_event.oid)
-    with pytest.raises(CloudFileNotFoundError):
-        provider.delete(oid=received_event.oid)
+    deleted_oid = received_event.oid
+    provider.delete(oid=deleted_oid)
+    provider.delete(oid=deleted_oid)  # Tests that deleting a non-existing file does not raise a FNFE
 
     received_event = None
     event_count = 0
@@ -220,16 +221,11 @@ def test_event_basic(util, provider: Provider):
     assert event_count == 1
     assert received_event is not None
     assert received_event.oid
-    path = received_event.path
-    if path is None:
-        try:
-            path = provider.info_oid(received_event.oid).path
-            assert path == dest
-        except CloudFileNotFoundError:
-            if received_event.exists:
-                raise
-    assert received_event.mtime
     assert not received_event.exists
+    if received_event.path is not None:
+        assert received_event.path == dest
+    assert received_event.oid == deleted_oid
+    assert received_event.mtime
 
 
 def test_api_failure(provider):
@@ -238,8 +234,160 @@ def test_api_failure(provider):
     # b) does not trap CloudTemporaryError's
 
     def side_effect(*a, **k):
+        if a or k:
+            pass
         raise CloudTemporaryError("fake disconnect")
 
     with patch.object(provider, "_api", side_effect=side_effect):
         with pytest.raises(CloudTemporaryError):
             provider.exists_path("/notexists")
+
+
+def test_file_not_found(provider):
+    # Test that operations on nonexistent file system objects raise CloudFileNotFoundError
+    # when appropriate, and don't when inappropriate
+    # provider.temp_name = lambda x: "/" + x  # TODO: fix this when we replace the mock fixture with the provider fixture
+    dat = os.urandom(32)
+
+    def data():
+        return BytesIO(dat)
+
+    test_path_deleted = provider.temp_name("dest1")  # Created, then deleted
+    info1 = provider.create(test_path_deleted, data())
+    test_oid_deleted = info1.oid
+    provider.delete(test_oid_deleted)
+
+    test_path_made_up = provider.temp_name("dest2")  # Never created
+    test_oid_made_up = "never created"
+    # TODO: consider mocking info_path to always return None, and then call all the provider methods
+    #  to see if they are handling the None, and not raising exceptions other than FNF
+
+    # Tests:
+    #   exists_path
+    #       returns false, does not raise
+    assert provider.exists_path(test_path_deleted) is False
+    assert provider.exists_path(test_path_made_up) is False
+
+    #   exists_oid
+    #       returns false, does not raise
+    assert provider.exists_oid(test_oid_deleted) is False
+    assert provider.exists_oid(test_oid_made_up) is False
+
+    #   info_path
+    #       deleted file returns None
+    #       never existed file returns None
+    assert provider.info_path(test_path_deleted) is None
+    assert provider.info_path(test_path_made_up) is None
+
+    #   info_oid
+    #       deleted file returns None
+    #       never existed file returns None
+    assert provider.info_oid(test_oid_deleted) is None
+    assert provider.info_oid(test_oid_made_up) is None
+
+    #   hash_oid
+    #       deleted file returns None
+    #       never existed file returns None
+    if getattr(provider, "hash_oid", False): # TODO implement hash_oid in gdrive, then don't have this be conditional
+        assert provider.hash_oid(test_oid_deleted) is None
+        assert provider.hash_oid(test_oid_made_up) is None
+
+    #   upload
+    #       to a deleted file raises FNF
+    #       to a made up oid raises FNF
+    # TODO: uploading to a deleted file might not raise an FNF, it might just untrash the file
+    with pytest.raises(CloudFileNotFoundError):
+        provider.upload(test_oid_deleted, data())
+    with pytest.raises(CloudFileNotFoundError):
+        provider.upload(test_oid_made_up, data())
+
+    #   create
+    #       to a non-existent folder, conditionally raises FNF
+    if not provider.auto_vivify_parent_folders:
+        with pytest.raises(CloudFileNotFoundError):
+            provider.create("/nonexistentfolder/junk", data())
+
+    #   download
+    #       on a deleted oid raises FNF
+    #       on a made up oid raises FNF
+    with pytest.raises(CloudFileNotFoundError):
+        provider.download(test_oid_deleted, data())
+    with pytest.raises(CloudFileNotFoundError):
+        provider.download(test_oid_made_up, data())
+
+    #   rename
+    #       from a deleted oid raises FNF
+    #       from a made up oid raises FNF
+    #       to a non-existent folder raises [something], conditionally
+    #       check the rename source to see if there are others
+    with pytest.raises(CloudFileNotFoundError):
+        provider.rename(test_oid_deleted, test_path_deleted)
+    with pytest.raises(CloudFileNotFoundError):
+        provider.rename(test_oid_made_up, test_path_made_up)
+
+    #   mkdir
+    #       to a non-existent folder raises [something], conditionally
+    if not provider.auto_vivify_parent_folders:
+        with pytest.raises(CloudFileNotFoundError):
+            provider.mkdir("/nonexistentfolder/junk")
+
+    #   delete
+    #       on a deleted oid does not raise
+    #       on a made up oid does not raise
+    provider.delete(test_oid_deleted)
+    provider.delete(test_oid_made_up)
+
+    #   listdir
+    #       raises FNF
+    with pytest.raises(CloudFileNotFoundError):
+        provider.listdir(test_path_deleted)
+    with pytest.raises(CloudFileNotFoundError):
+        provider.listdir(test_path_made_up)
+
+    # Google drive raises FNF when it can't find the root... can we test for that here?
+
+
+def test_file_exists(provider: Provider):
+    # Setup the initial state of the provider ==========================================
+    dat = os.urandom(32)
+
+    def data(da=dat):
+        return BytesIO(da)
+
+    test_folder = os.urandom(8).hex()
+    test_file = provider.temp_name()
+    # oid_dir = provider.mkdir(test_folder)
+    # info_file = provider.create(test_file, data())
+    # Setup the initial state of the provider ==========================================
+
+    # Test that operations on existent file system objects raise CloudExistsError
+    # when appropriate, and don't when inappropriate
+    # api functions to check for FileExists:
+    #   mkdir,
+    #       where target path has a parent folder that already exists as a file, raises FEx
+    #       where target path exists as a file, raises FEx
+    #       where target path exists as a folder, does not raise
+    #       creating a file, deleting it, then creating a folder at the same path, should not raise an FEx
+    #       creating a folder, deleting it, then creating a folder at the same path, should not raise an FEx
+    #   upload,
+    #       where target OID is a folder, raises FEx
+    #   create,
+    #       where target path exists, raises FEx
+    #       creating a file, deleting it, then creating a file at the same path, should not raise an FEx
+    #       creating a folder, deleting it, then creating a file at the same path, should not raise an FEx
+    #       where target path has a parent folder that already exists as a file, raises FEx
+    #   rename,
+    #       rename over empty folder succeeds
+    #       rename over non-empty folder raises FEx
+    #       target has a parent folder that already exists as a file, raises FEx
+    #       renaming file over a folder, raises FEx
+    #       renaming a folder over a file, raises FEx
+    #       create a file, delete it, then rename a file to the same path as the deleted, does not raise
+    #       create a folder, delete it, then rename file to the same path as the deleted, does not raise
+    #       create a file, delete it, then rename a folder to the same path as the deleted, does not raise
+    #       create a folder, delete it, then rename folder to the same path as the deleted, does not raise
+
+
+
+# TODO: test that renaming A over B replaces B's OID with A's OID, and B's OID is trashed
+
