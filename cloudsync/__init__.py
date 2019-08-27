f0d0454867ced31d74442ed0ba3c339f9f3a4a3f
erik aronesty <erik@q32.com>
2019-08-27 12:58:39 -0400

    .

--- cloudsync/__init__.py
+++ cloudsync/__init__.py
@@ -23,7 +23,7 @@ cloudsync -p gdrive --token "236723782347823642786" -f ~/gdrive-folder --daemon
 
 """
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 
 # must be imported first
 from .log import logger
