9055ec8cdc75c1b70547e5add975be55cfbe06fb
erik <erik@q32.com>
2019-09-04 16:10:21 -0400

    bumpver

---
+++ check_version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if git diff origin/master --name-only | grep -qE 'cloudsync/.*\.py'; then
+    if git diff origin/master | grep -q __version__; then
+        echo "OK, version changed"
+    else
+        echo "FAIL: please increment version"
+        exit 1
+    fi
+else
+    echo "Skipping version test because no py files changed"
+fi
