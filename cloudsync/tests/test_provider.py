d07881d1a889154aa56497bdd96862823de99099
earonesty <erik@q32.com>
2019-09-17 09:39:39 -0400

    Time help2 (#84)
    If the time helper raises an error, then all the tests will fail if they time out. The only test that was relying on silent timeouts was test_event_basic

--- cloudsync/tests/test_provider.py
+++ cloudsync/tests/test_provider.py
@@ -537,27 +537,29 @@ def test_event_basic(provider: ProviderMixin):
     done = False
     waiting = None
     wait_secs = min(provider.event_sleep * wait_sleep_cycles, 2)
-    for e in provider.events_poll(until=lambda: done):
-        log.debug("got event %s", e)
-        # you might get events for the root folder here or other setup stuff
-        if e.exists:
-            if not e.path:
-                info = provider.info_oid(e.oid)
-                if info:
-                    e.path = info.path
 
-            if e.path == dest:
-                received_event = e
-                event_count += 1
+    with pytest.raises(TimeoutError):
+        for e in provider.events_poll(until=lambda: done):
+            log.debug("got event %s", e)
+            # you might get events for the root folder here or other setup stuff
+            if e.exists:
+                if not e.path:
+                    info = provider.info_oid(e.oid)
+                    if info:
+                        e.path = info.path
 
-            log.debug("%s vs %s", e.path, dest)
+                if e.path == dest:
+                    received_event = e
+                    event_count += 1
 
-            if e.path == dest and not waiting:
-                waiting = time.monotonic() + wait_secs
+                log.debug("%s vs %s", e.path, dest)
 
-        if waiting and time.monotonic() > waiting:
-            # wait for extra events up to 10 sleep cycles, or 2 seconds
-            done = True
+                if e.path == dest and not waiting:
+                    waiting = time.monotonic() + wait_secs
+
+            if waiting and time.monotonic() > waiting:
+                # wait for extra events up to 10 sleep cycles, or 2 seconds
+                done = True
 
     assert event_count == 1
     assert received_event is not None
