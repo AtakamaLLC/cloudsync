cb06ecec9c1a612938b1ca11f57c68f4e5429ebc
earonesty <erik@q32.com>
2019-09-18 11:57:57 -0400

    Reorganize fixtures (#88)
    * reorganize some fixtures
    
    * .

--- cloudsync/tests/fixtures/util.py
+++ cloudsync/tests/fixtures/util.py
@@ -1,71 +1,65 @@
-import pytest
-import os
-import tempfile
-import shutil
+from typing import NamedTuple
 from inspect import getframeinfo, stack
 import logging
 from cloudsync.provider import Provider
+from cloudsync import SyncManager, CloudFileNotFoundError
 
 log = logging.getLogger(__name__)
 
 log.setLevel(logging.INFO)
 
+TIMEOUT = 4
 
-class Util:
-    def __init__(self):
-        self.base = tempfile.mkdtemp(suffix=".cloudsync")
-        log.debug("temp files will be in: %s", self.base)
+class WaitFor(NamedTuple):
+    side: int = None
+    path: str = None
+    hash: bytes = None
+    oid: str = None
+    exists: bool = True
 
-    @staticmethod
-    def get_context(level):
-        caller = getframeinfo(stack()[level+1][0])
-        return caller
+class RunUntilHelper:
+    def run_until_found(self: SyncManager, *files, timeout=TIMEOUT):
+        log.debug("running until found")
+        last_error = None
 
-    def temp_file(self, *, fill_bytes=None):
-        # pretty names for temps
-        caller = self.get_context(1)
-        fn = os.path.basename(caller.filename)
-        if not fn:
-            fn = "unk"
-        else:
-            fn = os.path.splitext(fn)[0]
+        def found():
+            ok = True
 
-        func = caller.function
+            for info in files:
+                if type(info) is tuple:
+                    info = WaitFor(side=info[0], path=info[1])
 
-        name = fn + '-' + func + "." + os.urandom(16).hex()
+                try:
+                    other_info = self.providers[info.side].info_path(info.path)
+                except CloudFileNotFoundError:
+                    other_info = None
 
-        fp = Provider.join(self.base, name)
+                if other_info is None:
+                    nonlocal last_error
+                    if info.exists is False:
+                        log.debug("waiting not exists %s", info.path)
+                        continue
+                    log.debug("waiting exists %s", info.path)
+                    last_error = CloudFileNotFoundError(info.path)
+                    ok = False
+                    break
 
-        if fill_bytes is not None:
-            with open(fp, "wb") as f:
-                f.write(os.urandom(fill_bytes))
+                if info.exists is False:
+                    ok = False
+                    break
 
-        log.debug("temp file %s", fp)
+                if info.hash and info.hash != other_info.hash:
+                    log.debug("waiting hash %s", info.path)
+                    ok = False
+                    break
 
-        return fp
+            return ok
 
-    def do_cleanup(self):
-        shutil.rmtree(self.base)
+        self.run(timeout=timeout, until=found)
 
+        if not found():
+            if last_error:
+                raise TimeoutError("timed out while waiting: %s" % last_error)
+            else:
+                raise TimeoutError("timed out while waiting")
 
-@pytest.fixture(scope="module")
-def util(request):
-    # user can override at the module level or class level
-    # if tehy want to look at the temp files made
-
-    cleanup = getattr(getattr(request, "cls", None), "util_cleanup", True)
-    if cleanup:
-        cleanup = getattr(request.module, "util_cleanup", True)
-
-    u = Util()
-
-    yield u
-
-    if cleanup:
-        u.do_cleanup()
-
-
-def test_util(util):
-    log.setLevel(logging.DEBUG)
-    f = util.temp_file(fill_bytes=32)
-    assert len(open(f, "rb").read()) == 32
