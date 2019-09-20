ef5e87d6d93b1d7406e0a9eff1f0b9f2b90e98a0
erik aronesty <erik@q32.com>
2019-09-20 16:49:50 -0400

    lindt

--- cloudsync/utils.py
+++ cloudsync/utils.py
@@ -1,37 +1,33 @@
 import logging
 from hashlib import md5
 from base64 import b64encode
-from typing import Any, Union
+from typing import Any, Union, List, Dict
 
 log = logging.getLogger(__name__)
 
 
 MAX_DEBUG_STR = 64
 
-def _debug_arg(v):
-    if isinstance(v, dict):
-        r = {}
-        for k, v in v.items():
+def _debug_arg(val: Any):
+    if isinstance(val, dict):
+        r: Dict[Any, Any] = {}
+        for k, v in val.items():
             r[k] = _debug_arg(v)
-        return r
-
-    if isinstance(v, str):
-        if len(v) > 64:
-            return v[0:61] + "..."
-        return v
-
-    if isinstance(v, bytes):
-        if len(v) > 64:
-            return v[0:61] + b"..."
-        return v
-
-    try:
-        r = []
-        for v in iter(v):
-            r.append(_debug_arg(v))
-        return r
-    except TypeError:
-        return v
+    elif isinstance(val, str):
+        if len(val) > 64:
+            val = val[0:61] + "..."
+    elif isinstance(val, bytes):
+        if len(val) > 64:
+            val = val[0:61] + b"..."
+    else:
+        try:
+            rlist: List[Any] = []
+            for v in iter(val):
+                rlist.append(_debug_arg(v))
+            val = rlist
+        except TypeError:
+            return val
+    return val
 
 # prevents very long arguments from getting logged
 def debug_args(*stuff: Any):
@@ -40,6 +36,7 @@ def debug_args(*stuff: Any):
         if len(r) == 1:
             r = r[0]
         return r
+    return "N/A"
 
 # useful for converting oids and pointer nubmers into digestible nonces
 def debug_sig(t: Any, size: int = 3) -> Union[str, int]:
