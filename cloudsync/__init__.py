aeb064d0599d5d65083590c95713c8117cb5fb25
erik <erik@q32.com>
2019-09-30 16:44:14 -0400

    .

--- cloudsync/__init__.py
+++ cloudsync/__init__.py
@@ -23,7 +23,7 @@ cloudsync -p gdrive --token "236723782347823642786" -f ~/gdrive-folder --daemon
 
 """
 
-__version__ = "0.2.23"
+__version__ = "0.2.24"
 
 from pystrict import strict, StrictError
 
