7ca4035d4e0df2605f080bde66d7510be6e3a8f5
erik <erik@q32.com>
2019-10-01 14:38:36 -0400

    try 3

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,12 @@ VERSION=$(./verok.py "$(git describe --abbrev=0 --tags)")
 
 echo ver $VERSION
 
+# if this is changed, also change the .gitignore script
 sed -i.bak "s/%VERSION%/$VERSION/" cloudsync/__init__.py
 
 cat cloudsync/__init__.py
 
 flit publish
 
+# it's nice to put things back, because reasons
 mv cloudsync/__init__.py.bak cloudsync/__init__.py
