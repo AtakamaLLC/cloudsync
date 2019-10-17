d5266b6b71a175af0d10c6e67643a30f1580186a
erik <erik@q32.com>
2019-10-17 17:58:31 -0400

    got basics of command line working

--- cloudsync/command.py
+++ cloudsync/command.py
@@ -1,6 +1,7 @@
-import os
+import sys
 import argparse
 import msgpack
+from .types import IgnoreReason
 
 def do_debug(args):
     if args.state:
@@ -18,10 +19,18 @@ def do_debug(args):
         store = SqliteStorage(args.state)
         tag_comma = ""
         for tag, ent in store.read_all().items():
-            print(tag_comma, '{"tag":"', tag, '","entries":[')
+            if args.json:
+                print(tag_comma, '{"tag":"', tag, '","entries":[')
             ent_comma = ""
             for (eid, ser) in ent.items():
                 if type(ser) is bytes:
+                    ent = SyncEntry(parent=fake_state, otype=None, storage_init=(eid, ser))
+                    if args.changed:
+                        if not ent[0].changed and not ent[1].changed:
+                            continue
+                    if ent.ignored != IgnoreReason.NONE and not args.discarded:
+                        continue
+
                     if args.json:
                         d = msgpack.loads(ser, use_list=True, raw=False)
 
@@ -42,13 +51,14 @@ def do_debug(args):
                         print(ent_comma, json.dumps(d))
                         ent_comma = ","
                     else:
-                        ent = SyncEntry(parent=fake_state, otype=None, storage_init=(eid, ser))
                         print(ent.pretty())
-            print("]}")
+            if args.json:
+                print("]}")
             tag_comma = ","
 
         if args.json:
             print("]")
+
 
 def main():
     parser = argparse.ArgumentParser(description='cloudsync - monitor and sync between cloud providers')
@@ -57,6 +67,8 @@ def main():
 
     debug_sub = cmds.add_parser('debug', help='Debug commands')
     debug_sub.add_argument('-s', '--state', help='Debug state file', action="store")
+    debug_sub.add_argument('-c', '--changed', help='Only changed records', action="store_true")
+    debug_sub.add_argument('-d', '--discarded', help='Show discarded records', action="store_true")
     debug_sub.add_argument('-j', '--json', help='Output as json', action="store_true")
     debug_sub.set_defaults(func=do_debug)
 
@@ -64,10 +76,8 @@ def main():
 
     print("# args", args.__dict__, file=sys.stderr)
 
+    if "func" not in args:
+        parser.print_help(file=sys.stderr)
+        sys.exit(1)
+
     args.func(args)
-
-
-if __name__ == "__main__":
-    import sys
-    sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
-    main()
