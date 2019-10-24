c8fdb2a6b9450fe43ab6d43dc0bc72fece205116
Michael Krebs <michael@vidaidentity.com>
2019-10-24 15:29:53 -0400

    handle sqlite3.operationalerror

--- cloudsync/sync/sqlite_storage.py
+++ cloudsync/sync/sqlite_storage.py
@@ -9,33 +9,45 @@ log = logging.getLogger(__name__)
 class SqliteStorage(Storage):
     def __init__(self, filename: str):
         self._filename = filename
+        self.db = self.__db_connect()
         self._ensure_table_exists()
 
-    def _ensure_table_exists(self):
+    def __db_connect(self):
         self.db = sqlite3.connect(self._filename,
                                   uri=self._filename.startswith('file:'),
                                   check_same_thread=self._filename == ":memory:",
                                   timeout=5,
                                   isolation_level=None,
                                   )
-        self.db.execute("PRAGMA journal_mode=WAL;")
-        self.db.execute("PRAGMA busy_timeout=5000;")
+        return self.db
+
+    def __db_execute(self, sql, parameters=()):
+        try:
+            retval = self.db.execute(sql, parameters)
+        except sqlite3.OperationalError:
+            self.__db_connect()  # reconnect
+            retval = self.db.execute(sql, parameters)
+        return retval
+
+    def _ensure_table_exists(self):
+        self.__db_execute("PRAGMA journal_mode=WAL;")
+        self.__db_execute("PRAGMA busy_timeout=5000;")
 
         # Not using AUTOINCREMENT: http://www.sqlitetutorial.net/sqlite-autoincrement/
-        self.db.execute('CREATE TABLE IF NOT EXISTS cloud (id INTEGER PRIMARY KEY, '
+        self.__db_execute('CREATE TABLE IF NOT EXISTS cloud (id INTEGER PRIMARY KEY, '
                         'tag TEXT NOT NULL, serialization BLOB)')
-        self.db.execute('CREATE INDEX IF NOT EXISTS cloud_tag_ix on cloud(tag)')
-        self.db.execute('CREATE INDEX IF NOT EXISTS cloud_id_ix on cloud(id)')
+        self.__db_execute('CREATE INDEX IF NOT EXISTS cloud_tag_ix on cloud(tag)')
+        self.__db_execute('CREATE INDEX IF NOT EXISTS cloud_id_ix on cloud(id)')
 
     def create(self, tag: str, serialization: bytes) -> Any:
         assert tag is not None
-        db_cursor = self.db.execute('INSERT INTO cloud (tag, serialization) VALUES (?, ?)',
+        db_cursor = self.__db_execute('INSERT INTO cloud (tag, serialization) VALUES (?, ?)',
                                     [tag, serialization])
         eid = db_cursor.lastrowid
         return eid
 
     def update(self, tag: str, serialization: bytes, eid: Any) -> int:
-        db_cursor = self.db.execute('UPDATE cloud SET serialization = ? WHERE id = ? AND tag = ?',
+        db_cursor = self.__db_execute('UPDATE cloud SET serialization = ? WHERE id = ? AND tag = ?',
                                     [serialization, eid, tag])
         ret = db_cursor.rowcount
         if ret == 0:
@@ -43,7 +55,7 @@ class SqliteStorage(Storage):
         return ret
 
     def delete(self, tag: str, eid: Any):
-        db_cursor = self.db.execute('DELETE FROM cloud WHERE id = ? AND tag = ?',
+        db_cursor = self.__db_execute('DELETE FROM cloud WHERE id = ? AND tag = ?',
                                     [eid, tag])
         if db_cursor.rowcount == 0:
             log.debug("ignoring delete: id %s doesn't exist", eid)
@@ -53,10 +65,10 @@ class SqliteStorage(Storage):
         ret = {}
         if tag is not None:
             query = 'SELECT id, tag, serialization FROM cloud WHERE tag = ?'
-            db_cursor = self.db.execute(query, [tag])
+            db_cursor = self.__db_execute(query, [tag])
         else:
             query = 'SELECT id, tag, serialization FROM cloud'
-            db_cursor = self.db.execute(query)
+            db_cursor = self.__db_execute(query)
 
         for row in db_cursor.fetchall():
             eid, row_tag, row_serialization = row
@@ -69,7 +81,7 @@ class SqliteStorage(Storage):
         return ret
 
     def read(self, tag: str, eid: Any) -> Optional[bytes]:
-        db_cursor = self.db.execute('SELECT serialization FROM cloud WHERE id = ? and tag = ?', [eid, tag])
+        db_cursor = self.__db_execute('SELECT serialization FROM cloud WHERE id = ? and tag = ?', [eid, tag])
         for row in db_cursor.fetchall():
             return row
         return None
