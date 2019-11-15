8e459d83f9823457e5efdfcf8163ba71b34d83eb
erik <erik@q32.com>
2019-11-15 09:32:09 -0500

    .

--- test_providers.sh
+++ test_providers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if git diff origin/master --name-only | grep -qE '(cloudsync/provider.py|cloudsync/providers/|test_provider)'; then
-    pytest --cov=cloudsync --rcfile=.coveragerc-integ --durations=0 -n=4 cloudsync/tests/test_provider.py -k "$1"
+    pytest --cov=cloudsync --cov-config=.coveragerc-integ --durations=0 -n=4 cloudsync/tests/test_provider.py -k "$1"
 else
     echo "Skipping integration test because no provider.py|providers/ changes"
 fi
