67c4ff8e11b9fac2e428dd742aa44172bf029305
erik <erik@q32.com>
2019-12-20 12:47:12 -0500

    fix oid as path merge conflict issue

--- cloudsync/sync/state.py
+++ cloudsync/sync/state.py
@@ -956,15 +956,17 @@ class SyncState:  # pylint: disable=too-many-instance-attributes, too-many-publi
             if prior_ent and not prior_ent.is_discarded:
                 if not ent or not ent.is_conflicted:
                     # reuse prior_ent
-                    log.debug("ent is %s, abandon it", ent)
-                    copy = None
+                    _copy = None
                     if ent:
                         # copy information about the other side
                         if ent[1-side].oid:
-                            copy = ent[1-side]
+                            _copy = ent[1-side]
                     ent = prior_ent
-                    if copy:
-                        ent[1-side] = copy
+                    if _copy:
+                        log.debug("ent was abandoned with copy")
+                        ent[1-side] = _copy
+                    else:
+                        log.debug("ent was abandoned without copy")
             elif not ent:
                 path_ents = self.lookup_path(side, path, stale=True)
                 for path_ent in path_ents:
