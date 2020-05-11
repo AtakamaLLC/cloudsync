5cb9252d572ca68e8df731e6647f009f948c9701
reed-atakama <59745433+reed-atakama@users.noreply.github.com>
2020-05-11 08:12:24 -0400

    BI2-1527: Create parent directory on rename with missing parent (#271)

--- cloudsync/sync/manager.py
+++ cloudsync/sync/manager.py
@@ -680,47 +680,7 @@ class SyncManager(Runnable):
             self._create_synced(changed, sync, translated_path)
             return FINISHED
         except ex.CloudFileNotFoundError:
-            # parent presumably exists
-            parent = self.providers[changed].dirname(sync[changed].path)
-            log.debug("make %s first before %s", parent, sync[changed].path)
-            ents = self.state.lookup_path(changed, parent)
-            if not ents:
-                info = self.providers[changed].info_path(parent)
-                if info:
-                    self.state.update(changed, DIRECTORY, info.oid, path=parent)
-                else:
-                    log.info("no info and no dir, ignoring?")
-
-            else:
-                parent_ent = ents[0]
-                if not parent_ent[changed].changed or not parent_ent.is_creation(changed):
-                    if sync.priority <= 2:  # punt if not already punted, meaning, punt at least once
-                        log.debug("Provider %s parent folder %s reported missing. punting", self.providers[synced].name, parent)
-                        return PUNT
-                    if parent_ent[changed].exists == EXISTS:
-                        # this condition indicates the provider has said the parent folder
-                        # doesn't exist, but the statedb says it does exist. First,
-                        # double-check using info_oid to see if the the parent DOES in fact exist
-                        # even though we got a FNF error before. Providers can take some time to
-                        # process a rename or create, so if we rename/create the parent folder,
-                        # the exists check on the path may still return false, even though an
-                        # exists check on the oid may reveal it does actually exist with the
-                        # correct path
-                        parent_info = self.providers[synced].info_oid(parent_ent[synced].oid)
-                        sync_parent = self.translate(synced, parent)
-                        if parent_info and parent_info.path == sync_parent:
-                            log.debug("Provider %s parent folder %s misreported missing, but parent folder exists. "
-                                      "punting", self.providers[synced].name, parent)
-                        else:
-                            # oddly, everything we know about the file is that it exists, but
-                            # the provider insists it doesn't
-                            # Clear the sync_path, and set synced to MISSING,
-                            # that way, we will recognize that this dir needs to be created
-                            parent_ent[changed].sync_path = None
-                            parent_ent[changed].changed = True
-                            parent_ent[synced].exists = MISSING
-                            assert parent_ent.is_creation(changed), "%s is not a creation" % parent_ent
-                            log.debug("updated entry as missing %s", parent)
+            return self.handle_cloud_file_not_found_error(changed, sync, synced)
         except ex.CloudFileExistsError:
             # there's a file or folder in the way, let that resolve if possible
             log.debug("can't create %s, try punting", translated_path)
@@ -745,6 +705,56 @@ class SyncManager(Runnable):
         except ex.CloudFileNameError:
             self.handle_file_name_error(sync, synced, translated_path)
             return FINISHED
+        return PUNT
+
+    def handle_cloud_file_not_found_error(self, changed, sync, synced):
+        if sync.priority > 5:
+            log.exception("punted too many times on CloudFileNotFoundError, giving up")
+            return FINISHED
+
+        # parent presumably exists
+        parent = self.providers[changed].dirname(sync[changed].path)
+        log.debug("make %s first before %s", parent, sync[changed].path)
+        ents = self.state.lookup_path(changed, parent)
+        if not ents:
+            info = self.providers[changed].info_path(parent)
+            if info:
+                self.state.update(changed, DIRECTORY, info.oid, path=parent)
+            else:
+                log.info("no info and no dir, ignoring")
+
+        else:
+            parent_ent = ents[0]
+            if not parent_ent[changed].changed or not parent_ent.is_creation(changed):
+                if sync.priority <= 2:  # punt if not already punted, meaning, punt at least once
+                    log.debug("Provider %s parent folder %s reported missing. punting", self.providers[synced].name, parent)
+                    return PUNT
+                if parent_ent[changed].exists == EXISTS:
+                    # this condition indicates the provider has said the parent folder
+                    # doesn't exist, but the statedb says it does exist. First,
+                    # double-check using info_oid to see if the the parent DOES in fact exist
+                    # even though we got a FNF error before. Providers can take some time to
+                    # process a rename or create, so if we rename/create the parent folder,
+                    # the exists check on the path may still return false, even though an
+                    # exists check on the oid may reveal it does actually exist with the
+                    # correct path
+                    parent_info = self.providers[synced].info_oid(parent_ent[synced].oid)
+                    sync_parent = self.translate(synced, parent)
+                    if parent_info and parent_info.path == sync_parent:
+                        log.debug("Provider %s parent folder %s misreported missing, but parent folder exists. "
+                                  "punting", self.providers[synced].name, parent)
+                    else:
+                        # oddly, everything we know about the file is that it exists, but
+                        # the provider insists it doesn't
+                        # Clear the sync_path, and set synced to MISSING,
+                        # that way, we will recognize that this dir needs to be created
+                        parent_ent[changed].sync_path = None
+                        parent_ent[changed].changed = True
+                        parent_ent[synced].exists = MISSING
+                        assert parent_ent.is_creation(changed), "%s is not a creation" % parent_ent
+                        log.debug("updated entry as missing %s", parent)
+
+        log.debug("CloudFileNotFoundError, punt")
         return PUNT
 
     def handle_file_name_error(self, sync, synced, translated_path):
@@ -1171,12 +1181,7 @@ class SyncManager(Runnable):
             new_oid = self.providers[synced].rename(sync[synced].oid, translated_path)
         except ex.CloudFileNotFoundError as e:
             log.debug("ERROR: can't rename for now %s: %s", sync, repr(e))
-            if sync.priority > 5:
-                log.exception("punted too many times, giving up")
-                return FINISHED
-            else:
-                log.debug("fnf, punt")
-            return PUNT
+            return self.handle_cloud_file_not_found_error(changed, sync, synced)
         except ex.CloudFileExistsError:
             log.debug("can't rename, file exists")
             if sync.priority <= 0:
