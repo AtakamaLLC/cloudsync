cc78e68bf3912b2fa3d9888fc0ca2d1469f0f897
Ilya Ayzenberg <10301392+howufiger7@users.noreply.github.com>
2020-08-21 15:33:50 -0400

    check git status

--- test_providers.sh
+++ test_providers.sh
@@ -3,7 +3,14 @@
 # allow coverage to include provider dirs when running integration tests
 cp codecov-integ.yml codecov.yml
 
-if git diff origin/master --name-only | grep -qE '(cloudsync/provider.py|cloudsync/providers/|test_provider)'; then
+git fetch origin master
+git_result=$(git diff origin/master --name-only)
+if [ $? -ne 0 ]; then
+    echo "git diff failed"
+    exit 1
+fi
+
+if echo $git_result | grep -qE '(cloudsync/provider.py|cloudsync/providers/|test_provider)'; then
     pytest --cov=cloudsync --cov-report=xml --cov-config=.coveragerc-integ --durations=0 -n=4 cloudsync/tests/test_provider.py --provider "$1" --timeout=600
 else
     echo "Skipping integration test because no provider.py|providers/ changes"
