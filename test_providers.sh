5586fd9dbe98b68a9802045906d41eeedc659042
Ilya Ayzenberg <10301392+howufiger7@users.noreply.github.com>
2020-08-21 15:39:40 -0400

    quote

--- test_providers.sh
+++ test_providers.sh
@@ -10,7 +10,7 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-if echo $git_result | grep -qE '(cloudsync/provider.py|cloudsync/providers/|test_provider)'; then
+if echo "$git_result" | grep -qE '(cloudsync/provider.py|cloudsync/providers/|test_provider)'; then
     pytest --cov=cloudsync --cov-report=xml --cov-config=.coveragerc-integ --durations=0 -n=4 cloudsync/tests/test_provider.py --provider "$1" --timeout=600
 else
     echo "Skipping integration test because no provider.py|providers/ changes"
