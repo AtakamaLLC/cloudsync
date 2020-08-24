b3b8836acdc68251c67ddbe8d843d5a3e3da9027
Ilya Ayzenberg <10301392+howufiger7@users.noreply.github.com>
2020-08-24 12:17:08 -0400

    fix pylint and integration test script (#322)
    - pylint 2.6 did this: Add `raise-missing-from` check for exceptions that should have a cause
    - integration test script should fail when the `git diff` operation fails

--- test_providers.sh
+++ test_providers.sh
@@ -3,7 +3,17 @@
 # allow coverage to include provider dirs when running integration tests
 cp codecov-integ.yml codecov.yml
 
-if git diff origin/master --name-only | grep -qE '(cloudsync/provider.py|cloudsync/providers/|test_provider)'; then
+git fetch origin master
+git_result=$(git diff origin/master --name-only)
+if [ $? -eq 0 ]; then
+    echo "git diff origin/master --name-only"
+    echo "$git_result"
+else
+    echo "git diff failed"
+    exit 1
+fi
+
+if echo "$git_result" | grep -qE '(cloudsync/provider.py|cloudsync/providers/|test_provider)'; then
     pytest --cov=cloudsync --cov-report=xml --cov-config=.coveragerc-integ --durations=0 -n=4 cloudsync/tests/test_provider.py --provider "$1" --timeout=600
 else
     echo "Skipping integration test because no provider.py|providers/ changes"
