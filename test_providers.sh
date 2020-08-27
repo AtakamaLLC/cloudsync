04ee3c8d3dbfe1dd1d7772b87358625f1b5187ef
Ilya Ayzenberg <10301392+howufiger7@users.noreply.github.com>
2020-08-27 19:49:58 -0400

    final version of test_providers.sh

--- test_providers.sh
+++ test_providers.sh
@@ -3,20 +3,14 @@
 # allow coverage to include provider dirs when running integration tests
 cp codecov-integ.yml codecov.yml
 
-git_branch=$(git branch --show-current)
-if [ "$git_branch" = "" ]; then
-  echo "Detached state, skipping integration tests"
-  exit 0
-fi
-
 git fetch origin master
 git_result=$(git diff origin/master --name-only)
 if [ $? -eq 0 ]; then
     echo "git diff origin/master --name-only"
     echo "$git_result"
 else
-    echo "git diff failed"
-    exit 1
+    echo "git diff failed in detached state"
+    exit 0
 fi
 
 if echo "$git_result" | grep -qE '(cloudsync/provider.py|cloudsync/providers/|test_provider)'; then
