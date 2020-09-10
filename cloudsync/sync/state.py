bc1a235991e94da175685cda70dc1026aaf21c57
Michael Krebs <michael@vidaidentity.com>
2020-09-10 01:52:38 -0400

    improve prior ent condition another time more

--- cloudsync/sync/state.py
+++ cloudsync/sync/state.py
@@ -1014,8 +1014,7 @@ class SyncState:  # pylint: disable=too-many-instance-attributes, too-many-publi
                 ent.ignored = IgnoreReason.NONE
 
             if prior_ent and not prior_ent.is_discarded:
-                if not ent or not ent.is_conflicted \
-                        and (prior_ent[side].sync_hash or not ent[side].sync_hash):
+                if not ent or (not ent.is_conflicted and (prior_ent[side].sync_hash or not ent[side].sync_hash)):
                     # reuse prior_ent
                     _copy = None
                     if ent:
