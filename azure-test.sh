ef9638874dc3948de0ac945edc24ef1712de3f42
erik aronesty <erik@q32.com>
2020-09-14 11:48:53 -0400

    try to fix azure:

--- azure-test.sh
+++ azure-test.sh
@@ -4,6 +4,8 @@ set -o errexit
 
 pip install pytest pytest-azurepipelines
 
-pytest --durations=1 --cov=cloudsync  -n=2 cloudsync/tests cloudsync/oauth/apiserver.py --timeout=300
+pytest --durations=1 --cov=cloudsync  cloudsync/tests cloudsync/oauth/apiserver.py --timeout=300 &
 
-pytest --durations=1 --cov=cloudsync --cov-append --cov-report xml -n=2 cloudsync/tests/test_provider.py --provider=filesystem
+pytest --durations=1 --cov=cloudsync --cov-append --cov-report xml cloudsync/tests/test_provider.py --provider=filesystem &
+
+wait
