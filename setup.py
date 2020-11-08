dbac44e2f87384c967d0902670628bedda953a42
Michael Krebs <michael@vidaidentity.com>
2020-11-08 17:05:04 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='cloudsync',
+    version='2.4.5a',
+    packages=['cloudsync', 'cloudsync.sync', 'cloudsync.oauth', 'cloudsync.tests', 'cloudsync.tests.fixtures',
+              'cloudsync.command', 'cloudsync.providers', 'onedrivesdklink', 'onedrivesdklink.model',
+              'onedrivesdklink.helpers', 'onedrivesdklink.extensions', 'onedrivesdklink.version_bridge'],
+    url='',
+    license='',
+    author='Atakama',
+    author_email='',
+    description=''
+)
