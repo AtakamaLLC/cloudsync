813aea18b2dadf120563793f6a5246d637e000ee
Ilya Ayzenberg <10301392+howufiger7@users.noreply.github.com>
2022-06-01 21:22:39 -0400

    azure test fail

--- azure-test.sh
+++ azure-test.sh
@@ -4,8 +4,6 @@ set -o errexit
 
 pip install pytest pytest-azurepipelines
 
-pytest --durations=1 --cov=cloudsync  cloudsync/tests cloudsync/oauth/apiserver.py --timeout=300 &
+pytest --durations=1 --cov=cloudsync  cloudsync/tests cloudsync/oauth/apiserver.py --timeout=300
 
-pytest --durations=1 --cov=cloudsync --cov-append --cov-report xml cloudsync/tests/test_provider.py --provider=filesystem &
-
-wait
+pytest --durations=1 --cov=cloudsync --cov-append --cov-report xml cloudsync/tests/test_provider.py --provider=filesystem
