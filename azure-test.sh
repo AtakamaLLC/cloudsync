e52f01a1dafb9cbd69e1bc026aa22fc965563908
Ilya Ayzenberg <10301392+howufiger7@users.noreply.github.com>
2022-06-01 21:37:00 -0400

    async

--- azure-test.sh
+++ azure-test.sh
@@ -4,6 +4,11 @@ set -o errexit
 
 pip install pytest pytest-azurepipelines
 
-pytest --durations=1 --cov=cloudsync  cloudsync/tests cloudsync/oauth/apiserver.py --timeout=300
+pytest --durations=1 --cov=cloudsync  cloudsync/tests cloudsync/oauth/apiserver.py --timeout=300 &
+pid1=$!
 
-pytest --durations=1 --cov=cloudsync --cov-append --cov-report xml cloudsync/tests/test_provider.py --provider=filesystem
+pytest --durations=1 --cov=cloudsync --cov-append --cov-report xml cloudsync/tests/test_provider.py --provider=filesystem &
+pid2=$!
+
+wait $pid1
+wait $pid2
