b26c65aacda5e82bfac5ce2da8a57e2daac6a7b1
Michael Krebs <michael@vidaidentity.com>
2022-06-13 12:23:07 -0400

    add additional debugging output to azure test

--- azure-test.sh
+++ azure-test.sh
@@ -10,5 +10,10 @@ pid1=$!
 pytest --durations=1 --cov=cloudsync --cov-append --cov-report xml cloudsync/tests/test_provider.py --provider=filesystem &
 pid2=$!
 
+echo Waiting for "cloudsync/tests cloudsync/oauth/apiserver.py"
 wait $pid1
+
+echo Waiting for "cloudsync/tests/test_provider.py --provider=filesystem"
 wait $pid2
+
+echo Done
