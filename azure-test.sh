23d5262b398578990807810e66b5507ae9e6b83b
Ilya Ayzenberg <10301392+howufiger7@users.noreply.github.com>
2022-11-16 09:32:08 -0500

    fix azure script

--- azure-test.sh
+++ azure-test.sh
@@ -8,6 +8,7 @@ pytest --durations=1 --cov=cloudsync --cov-report=xml cloudsync/tests cloudsync/
 pid1=$!
 
 pytest --durations=1 --cov=cloudsync --cov-append --cov-report=xml cloudsync/tests/test_provider.py --provider=filesystem &
+pid2=$!
 
 echo Waiting for "cloudsync/tests cloudsync/oauth/apiserver.py"
 wait $pid1
